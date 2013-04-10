aa3e6968b175a5b55351f8d20b04745b9177dba3
dragonls <liushen001@126.com>
2013-04-10 20:20:56 +0800

    Finish

--- 2-python-practice/2-ngram-count/ngram-count.py
+++ 2-python-practice/2-ngram-count/ngram-count.py
@@ -4,7 +4,9 @@ class NGram(object):
 
     def __init__(self, n):
         # n is the order of n-gram language model
-        self.n = n
+	self.n = n
+	self.unigram=0
+	self.bigram=0
 
     # scan a sentence, extract the ngram and update their
     # frequence.
@@ -13,6 +15,8 @@ class NGram(object):
     # @return   none
     def scan(self, sentence):
         # file your code here
+	l = sentence.strip()
+	self.ngram( l.split() )
 
     # caluclate the ngram of the words
     #
@@ -20,8 +24,37 @@ class NGram(object):
     # @return   int         count of the ngram
     def ngram(self, words):
         # file your code here
-
+	for i in range(len(words)):
+		if(words[i]=="unigram"):
+			self.unigram=self.unigram+1
+		elif(words[i]=="bigram"):
+			self.bigram=self.bigram+1
 
 if __name__=="__main__":
-    import sys
-    print >> sys.stderr, "library is not runnable"
+	import sys
+	try:
+		fpi = open(sys.argv[1], "r")
+	except IOError:
+		print >> sys.stderr, "failed to open file."
+		sys.exit(1)
+
+	try:
+		fpou = open("data.uni", "w")
+	except IOError:
+		print >> sys.stderr, "failed to open data.uni."
+		sys.exit(1)
+	
+	try:
+		fpob = open("data.bi", "w")
+	except IOError:
+		print >> sys.stderr, "failed to open data.bi."
+		sys.exit(1)
+
+	N = NGram(1)
+	line = fpi.readline()
+	while line:
+		N.scan(line)
+		line = fpi.readline()
+	fpou.write(str(N.unigram))
+	fpob.write(str(N.bigram))
+
